8d42c73a6d548f6ca70b5947376ed358a7668a49
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2020-04-01 16:56:55 +0200

    create interactive maps

---
+++ src/create_interactive_maps.R
@@ -0,0 +1,184 @@
+# Create interactive html widget maps
+# by Pieterjan Verhelst
+# Pieterjan.Verhelst@UGent.be
+
+# Packages
+library(sf)
+library(tmap)
+library(rnaturalearth)
+library(rnaturalearthdata)
+devtools::install_github("ropensci/rnaturalearthhires")
+library(rnaturalearthhires)
+library(rgeos)
+
+#library(leaflet)
+#library(htmltools)
+#library(htmlwidgets)
+#library(plotly)
+
+
+# Source code to upload and process data
+source('./src/attach_stour.R')
+
+# Animal project codes
+unique(data$animal_project_code)
+#[1] "2004_Gudena"            "2013_albertkanaal"      "2012_leopoldkanaal"     "SEMP"                  
+#[5] "PTN-Silver-eel-Mondego" "2014_Frome"             "2015_phd_verhelst"      "ESGL"                  
+#[9] "2011_Warnow"            "DAK"                    "2019_Grotenete"         "Noordzeekanaal"        
+#[13] "2017_Fremur"            "2011_Loire"             "2013_stour"   
+
+
+
+# Upload world map with all countries
+# Plot Europe
+world <- ne_countries(scale = "large", returnclass = "sf")
+class(world)
+
+tm_shape(world) +
+  tm_fill() +
+  tm_borders() 
+
+# Set interactive view
+ttm() # switches between static plot and interactive viewing
+
+
+
+# Create subset per project
+
+# 2004_gudena ####
+gudena <- filter(data, animal_project_code == "2004_Gudena")
+gudena$day <- as.Date(gudena$date_time)
+gudena <- select(gudena, animal_project_code, scientific_name, date_time, day, tag_id, station_name, receiver_id, deploy_longitude, deploy_latitude)
+unique(gudena$tag_id) # 63 detected eels, 
+
+# Create sf
+spatial_gudena <- st_as_sf(gudena,
+                           coords = c(8:9),
+                           crs = 4326)  # WGS84
+
+# Select country of project
+# This enables the code to run faster, instead of using the 'world' data frame
+denmark <- filter(world, name_long == "Denmark")   
+
+# Create and save interactive map
+gudena_map <- tm_shape(denmark) +
+            tm_basemap() +    
+            tm_borders() +
+            tm_shape(spatial_gudena) + tm_dots(col = "day", palette = "Spectral", size = 0.5) +
+            tm_facets(by = "tag_id",  ncol = 2, nrow = 32) +
+            tmap_options(limits = c(facets.view = 63))
+gudena_map
+
+
+#=================================================================
+
+
+# 2013_albertkanaal ####
+albertkanaal <- filter(data, animal_project_code == "2013_albertkanaal")
+albertkanaal$day <- as.Date(albertkanaal$date_time)
+albertkanaal <- select(albertkanaal, animal_project_code, scientific_name, date_time, day, tag_id, station_name, receiver_id, deploy_longitude, deploy_latitude)
+unique(albertkanaal$tag_id) # 160 detected eels, 
+
+# Create sf
+spatial_albertkanaal <- st_as_sf(albertkanaal,
+                           coords = c(8:9),
+                           crs = 4326)  # WGS84
+
+# Select country of project
+# This enables the code to run faster, instead of using the 'world' data frame
+belgium <- filter(world, name_long == "Belgium")   
+
+# Create and save interactive map
+albertkanaal_map <- tm_shape(belgium) +
+  tm_basemap() +    
+  tm_borders() +
+  tm_shape(spatial_albertkanaal) + tm_dots(col = "day", palette = "Spectral", size = 0.5) +
+  tm_facets(by = "tag_id",  ncol = 2, nrow = 80) +
+  tmap_options(limits = c(facets.view = 160))
+albertkanaal_map
+
+
+#=================================================================
+
+
+# 2019_Grotenete ####
+grotenete <- filter(data, animal_project_code == "2019_Grotenete")
+grotenete$day <- as.Date(grotenete$date_time)
+grotenete <- select(grotenete, animal_project_code, scientific_name, date_time, day, tag_id, station_name, receiver_id, deploy_longitude, deploy_latitude)
+unique(grotenete$tag_id) # 7 detected eels, 
+
+# Create sf
+spatial_grotenete <- st_as_sf(grotenete,
+                                 coords = c(8:9),
+                                 crs = 4326)  # WGS84
+
+# Select country of project
+# This enables the code to run faster, instead of using the 'world' data frame
+belgium <- filter(world, name_long == "Belgium")   
+
+# Create and save interactive map
+grotenete_map <- tm_shape(belgium) +
+  tm_basemap() +    
+  tm_borders() +
+  tm_shape(spatial_grotenete) + tm_dots(col = "day", palette = "Spectral", size = 0.5) +
+  tm_facets(by = "tag_id",  ncol = 2, nrow = 4) +
+  tmap_options(limits = c(facets.view = 7))
+grotenete_map
+
+
+#=================================================================
+
+
+# 2014_Frome ####
+frome <- filter(data, animal_project_code == "2014_Frome")
+frome$day <- as.Date(frome$date_time)
+frome <- select(frome, animal_project_code, scientific_name, date_time, day, tag_id, station_name, receiver_id, deploy_longitude, deploy_latitude)
+unique(frome$tag_id) # 47 detected eels, 
+
+# Create sf
+spatial_frome <- st_as_sf(frome,
+                              coords = c(8:9),
+                              crs = 4326)  # WGS84
+
+# Select country of project
+# This enables the code to run faster, instead of using the 'world' data frame
+uk <- filter(world, name_long == "United Kingdom")   
+
+# Create and save interactive map
+frome_map <- tm_shape(uk) +
+  tm_basemap() +    
+  tm_borders() +
+  tm_shape(spatial_frome) + tm_dots(col = "day", palette = "Spectral", size = 0.5) +
+  tm_facets(by = "tag_id",  ncol = 2, nrow = 24) +
+  tmap_options(limits = c(facets.view = 48))
+frome_map
+
+
+#=================================================================
+
+
+
+
+
+
+
+
+
+
+library(mapview)
+library(leaflet)
+
+mapview(grotenete_map)
+
+
+m <- leaflet(gudena_map) %>%
+  addTiles() %>%
+  addMarkers(lng = 9,0,
+             lat = 55,0)
+m
+## create standalone .html
+mapshot(m, url = paste0(getwd(), "/map.html"))
+
+
+
+
