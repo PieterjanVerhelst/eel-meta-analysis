ec9c571ca5bb083c7bb0a33c36e092239a47ee83
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2020-04-02 14:03:01 +0200

    remove false detections warnow dataset

--- src/process_data.R
+++ src/process_data.R
@@ -24,8 +24,8 @@ data <- data[!(data$animal_project_code == "2012_leopoldkanaal" & data$station_n
 # 2014_Frome ####
 
 # False detections in the Albert Canal (S-stations)
-frome <- filter(data, animal_project_code == "2014_Frome")
-unique(frome$station_name)
+#frome <- filter(data, animal_project_code == "2014_Frome")
+#unique(frome$station_name)
 # [1] "F1"  "F2"  "F6"  "F7"  "F8"  "F5"  "F3"  "F4"  "S18" "S15" "S10" "S12"
 
 data <- data[!(data$animal_project_code == "2014_Frome" & data$station_name=="S10"),]
@@ -33,7 +33,6 @@ data <- data[!(data$animal_project_code == "2014_Frome" & data$station_name=="S1
 data <- data[!(data$animal_project_code == "2014_Frome" & data$station_name=="S15"),]
 data <- data[!(data$animal_project_code == "2014_Frome" & data$station_name=="S18"),]
 
-rm(frome)
 
 
 
@@ -53,86 +52,23 @@ data <- data[!(data$animal_project_code == "ESGL" & data$station_name=="HH5"),]
 
 
 
+# 2011_Warnow ####
+
+# False detections in Albert Canal (ak-29, ak-33, S14, S16, S18) and bpns (bpns-CNB03, PC4C-A7-3 and PC4C-B10-6)
+# warnow <- filter(data, animal_project_code == "2011_Warnow")
+# unique(warnow$station_name)
+#[1] "W5"         "W6"         "W2"         "W7"         "W8"         "W3"         "W4"         "W1"        
+#[9] "PC4C-B10-6" "S14"        "PC4C-A7-3"  "ak-33"      "S18"        "S16"        "ak-29"      "bpns-CNB03"
+
+data <- data[!(data$animal_project_code == "2011_Warnow" & data$station_name=="ak-29"),]
+data <- data[!(data$animal_project_code == "2011_Warnow" & data$station_name=="ak-33"),]
+data <- data[!(data$animal_project_code == "2011_Warnow" & data$station_name=="S14"),]
+data <- data[!(data$animal_project_code == "2011_Warnow" & data$station_name=="S16"),]
+data <- data[!(data$animal_project_code == "2011_Warnow" & data$station_name=="S18"),]
+data <- data[!(data$animal_project_code == "2011_Warnow" & data$station_name=="bpns-CNB03"),]
+data <- data[!(data$animal_project_code == "2011_Warnow" & data$station_name=="PC4C-A7-3"),]
+data <- data[!(data$animal_project_code == "2011_Warnow" & data$station_name=="PC4C-B10-6"),]
 
 
 
 
-
-
-data$deploy_latitude
-
-
-
-bh9 <- filter(data, station_name == "bh-9")
-
-
-
-
-
-
-leopoldkanaal <- filter(data, animal_project_code == "2012_leopoldkanaal")
-no_lon <- leopoldkanaal[is.na(leopoldkanaal$deploy_longitude),]
-
-sub_data <- filter(data, animal_project_code == "2012_leopoldkanaal" |
-                     animal_project_code == "2014_Frome")
-
-
-
-for (i in 1:dim(sub_data)[1]){
-  if (sub_data$animal_project_code[i] == "2012_leopoldkanaal" & sub_data$receiver_id[i] == "VR2W-115438" &
-      is.na(sub_data$station_name)){
-    sub_data$station_name[i] = "bh_test" 
-  }else{
-    sub_data$station_name[i] <- sub_data$station_name[i]
-  }
-}
-
-
-
-
-if (sub_data$animal_project_code == "2012_leopoldkanaal" & sub_data$receiver_id == "VR2W-115438" &
-      is.na(sub_data$station_name)){
-    sub_data$station_name = "bh-9" 
-  }else{
-    sub_data$station_name <- sub_data$station_name
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-sub_data2 <- sub_data %>% 
-  filter(animal_project_code == "2012_leopoldkanaal" , receiver_id == "VR2W-115438" , station_name == 'NA') %>%
-  mutate(height = replace(height, height == 20, NA))
-
-
-df[df$height == 20, "height"] <- NA 
-
-sub_data[sub_data$station_name == 'NA'] <- "bh_test"
-
-
-
-sub_data$station_name <- if(sub_data$animal_project_code == "2012_leopoldkanaal" & 
-                                   sub_data$receiver_id[i] == "VR2W-115438" &
-                                   is.na(sub_data$station_name), "bh-test", sub_data$station_name)  
-
-
-  
-  
-sub_data$station_name <- if(sub_data$animal_project_code == "2012_leopoldkanaal" & sub_data$receiver_id == "VR2W-115438" & is.na(sub_data$station_name)){
-    sub_data$station_name <- "bh_test"
-  }else{
-    sub_data$station_name <- sub_data$station_name
-  }
-                                
-                                
-
-no_lon2 <- sub_data[is.na(sub_data$deploy_longitude),]
