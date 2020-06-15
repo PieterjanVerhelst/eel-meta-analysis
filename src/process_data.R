f71393c1a91840b8e0ac2493411d671df79ff5a4
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2020-06-15 13:14:54 +0200

    remove false detections from emmn project

--- src/process_data.R
+++ src/process_data.R
@@ -119,5 +119,20 @@ data <- data[!(data$tag_id == "A69-1601-38745" & data$station_name=="13"),]
 
 
 
+# EMMN ####
+# False detections in various networks of Europe, all after study period (2007)
+data <- data[!(data$animal_project_code == "EMMN" & data$date_time >= '2008-01-01 00:00:00'),]
+
+# Remove detections from receivers with incorrect position (actual position is unknown)
+data <- data[!(data$animal_project_code == "EMMN" & data$receiver_id >= "VR2-5349"),]
+data <- data[!(data$animal_project_code == "EMMN" & data$receiver_id >= "VR2-5357"),]
+data <- data[!(data$animal_project_code == "EMMN" & data$receiver_id >= "VR2W-100569"),]
+data <- data[!(data$animal_project_code == "EMMN" & data$receiver_id >= "VR2W-100574"),]
+
+
+
+
+
+
 
 
