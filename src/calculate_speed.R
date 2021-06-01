68439655083e2872bda40e18f1740aef9a2bfd0c
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2021-06-01 23:12:08 +0200

    calculate speed

--- src/calculate_speed.R
+++ src/calculate_speed.R
@@ -15,19 +15,22 @@ library(actel)
 # Filter Gudena detection data
 gudena <- read_csv("./data/interim/residencies/residency_2004_gudena.csv")
 gudena$X1 <- NULL
-gudena$Array <- gudena$station_name
+#gudena$Array <- gudena$station_name
 
 # Load distance matrix
-gudena_distances <- read_csv("./data/external/distance_matrices/distancematrix_2004_gudena.csv")
-gudena_distances %<>% rename(station_name = X1)
+# Make sure the first column is not containing the station names
+gudena_distances <- read.csv("./data/external/distance_matrices/distancematrix_2004_gudena.csv",  row.names = 1)
+
 
 
 movementSpeeds <- function(movements, speed.method, dist.mat) {
   #appendTo("debug", "Running movementSpeeds.")
-  movements$Average.speed.m.s[1] <- NA
+  movements$swimtime_s[1] <- NA
+  movements$swimdistance_m[1] <- NA
+  movements$speed_m_s[1] <- NA
   if (nrow(movements) > 1) {
     capture <- lapply(2:nrow(movements), function(i) {
-      if (movements$Array[i] != movements$Array[i - 1] & all(!grep("^Unknown$", movements$Array[(i - 1):i]))) {
+      if (movements$station_name[i] != movements$station_name[i - 1] & all(!grep("^Unknown$", movements$station_name[(i - 1):i]))) {
         if (speed.method == "last to first"){
           a.sec <- as.vector(difftime(movements$arrival[i], movements$departure[i - 1], units = "secs"))
           my.dist <- dist.mat[movements$station_name[i], gsub(" ", ".", movements$station_name[i - 1])]
@@ -36,10 +39,12 @@ movementSpeeds <- function(movements, speed.method, dist.mat) {
           a.sec <- as.vector(difftime(movements$departure[i], movements$departure[i - 1], units = "secs"))
           my.dist <- dist.mat[movements$station_name[i], gsub(" ", ".", movements$station_name[i - 1])]
         }
-        movements$Average.speed.m.s[i] <<- round(my.dist/a.sec, 6)
+        movements$swimtime_s[i] <<- round(a.sec, 6)
+        movements$swimdistance_m[i] <<- round(my.dist, 6)
+        movements$speed_m_s[i] <<- round(my.dist/a.sec, 6)
         rm(a.sec, my.dist)
       } else {
-        movements$Average.speed.m.s[i] <<- NA_real_
+        movements$speed_m_s[i] <<- NA_real_
       }
     })
   }
