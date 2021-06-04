20b6c0d9a11c9d245bb146d22db1e0cedc8130bd
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2021-06-04 14:59:10 +0200

    calculate speed per tag id

--- src/calculate_speed.R
+++ src/calculate_speed.R
@@ -21,8 +21,22 @@ residency$X1 <- NULL
 # Make sure the first column is not containing the station names
 distance_matrix <- read.csv("./data/external/distance_matrices/distancematrix_2004_gudena.csv",  row.names = 1)
 
-# Calculate speed
-speed <- movementSpeeds(residency, "last to first", distance_matrix)
+# Calculate speed without taking into account different tag_id
+#speed <- movementSpeeds(residency, "last to first", distance_matrix)
+
+# Turn dataset into list per tag_id
+residency_list <- split(residency , f = residency$tag_id )
+#sapply(residency_list, function(x) max(x$detections))
+
+# Calculate speed per tag_id
+speed_list <- lapply(residency_list, function(x) movementSpeeds(x, "last to first", distance_matrix))
+#speed_list[[1]]
+
+# Turn lists back into dataframe
+#speed <- do.call(rbind.data.frame, speed)
+speed <- plyr::ldply (speed_list, data.frame)
+speed$.id <- NULL
+
 
 
 # Write csv
