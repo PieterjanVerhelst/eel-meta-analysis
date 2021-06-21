30aa1eaadbc6da0646a6c95b911d1b265e349e5d
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2021-06-21 20:29:20 +0200

    set uniform protocol notation

--- src/merge_eel_characteristics.R
+++ src/merge_eel_characteristics.R
@@ -13,17 +13,36 @@ eel$tag_id <- factor(eel$tag_id)
 eel$capture_date_time  <- as_datetime(eel$capture_date_time)
 eel$release_date_time  <- as_datetime(eel$release_date_time)
 
-# 2. Return number of tagged eels per project ####
+
+# 2. Remove eel from saeftinghe ####
+eel <- eel[!(eel$animal_project_code == "2015_phd_verhelst_eel" & eel$tag_id == "A69-1601-58620"),]
+
+# 3. Replace "Silver" by "silver" in life stage ####
+unique(eel$life_stage)
+eel <- eel %>%                               
+  mutate(life_stage = replace(life_stage, life_stage == "Silver", "silver"))
+
+
+# 4. Return number of tagged eels per project ####
 eel %>%
   group_by(animal_project_code) %>%
   summarise(tot_eels = n_distinct(tag_id))
 
 
-# 3. Merge eel characteristics with dataset ####
+# 5. Substitute code space into 'Vemco' format ####
+# 2011_Loire & part EMNN
+eel$tag_id <- gsub("R04K", "A69-1206", eel$tag_id)
+
+# 2017_Fremur & part EMMN
+eel$tag_id <- gsub("S256", "A69-1105", eel$tag_id)
+
+
+
+# 6. Merge eel characteristics with dataset ####
 data <- merge(data, eel, by=c("tag_id", "animal_project_code"))
 
 
-# 4. Return number of detected eels per year ####
+# 7. Return number of detected eels per year ####
 data %>%
   group_by(animal_project_code) %>%
   summarise(tot_eels = n_distinct(tag_id))
