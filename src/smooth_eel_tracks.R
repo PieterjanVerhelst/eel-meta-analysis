d566e8054550c950b9ed63597562eaa66fed245d
Pieterjan Verhelst <pieterjan.verhelst@inbo.be>
2022-03-02 11:09:21 +0100

    run code on project semp

--- src/smooth_eel_tracks.R
+++ src/smooth_eel_tracks.R
@@ -28,7 +28,7 @@ data$...1 <- NULL
 #[16] "ESGL"                   "life4fish"              "2013_Stour"     
 
 # Filter project detection data
-subset <- filter(data, animal_project_code == "life4fish")
+subset <- filter(data, animal_project_code == "SEMP")
 head(subset)
 remove(data)
 
@@ -39,7 +39,7 @@ subset$counts <- 1
 subset %<>% arrange(acoustic_tag_id, date_time)
 
 # Import distance matrix
-distance_matrix <- read_csv("./data/external/distance_matrices/distancematrix_life4fish.csv")
+distance_matrix <- read_csv("./data/external/distance_matrices/distancematrix_semp.csv")
 
 
 # Extract eel codes
@@ -145,5 +145,5 @@ residency <- residency[, c("animal_project_code", "acoustic_tag_id","station_nam
 
 
 # Write csv
-write.csv(residency, "./data/interim/residencies/residency_life4fish.csv")
+write.csv(residency, "./data/interim/residencies/residency_semp.csv")
 
