613ae5d42e9a638fe896c4490fce41733477092b
Pieterjan Verhelst <pieterjan.verhelst@inbo.be>
2022-04-05 11:50:23 +0200

    Update create_distance_plot.R

--- src/create_distance_plot.R
+++ src/create_distance_plot.R
@@ -4,38 +4,46 @@
 
 library(tidyverse)
 library(lubridate)
+library(tidyquant)
 
 
 # Upload dataset
-data <- read_csv('./data/interim/speed/speed_2004_gudena.csv') 
-data$X1 <- NULL
-data$tag_id <- factor(data$tag_id)
+data <- read_csv('./data/interim/speed/speed_2013_stour.csv') 
+data$...1 <- NULL
+data$...2 <- NULL
+data$acoustic_tag_id <- factor(data$acoustic_tag_id)
 
 # Create plot for all eels in single pdf
 #data$arrival <- ymd_hms(data$arrival)
 #data$arrival <- as.POSIXct(strptime(data$arrival,"%Y-%m-%d %H:%M:%S"))
 
+min(data$distance_to_source_m)  # Identify the min limit for the y-axis
+max(data$distance_to_source_m)  # Identify the max limit for the y-axis
 
-mydfnew.split.eel <- split(data, data$tag_id) # split dataset based on tag IDs
-pdf("./figures/distance_tracks/distance_tracks_2004_gudena.pdf") # Create pdf
+
+mydfnew.split.eel <- split(data, data$acoustic_tag_id) # split dataset based on tag IDs
+pdf("./figures/distance_tracks/distance_tracks_2013_stour.pdf") # Create pdf
 
 
 for (i in 1:length(mydfnew.split.eel)){ #i van 1 tot aantal transmitters
   mydfnew.temp<-mydfnew.split.eel[[i]] #for loop wordt doorlopen voor elke i transmitter
   g <- ggplot()
+  g <- g + theme(axis.text.x = element_text(size = 12, colour = "black", angle=90))
   g <- g + geom_line(aes(arrival, distance_to_source_m), data = mydfnew.temp, colour = "black", size = 1)
   g <- g + geom_point(aes(arrival, distance_to_source_m), data = mydfnew.temp, shape = 1, size = 5, colour = "black")
   g <- g + theme(plot.title = element_text(lineheight=.8, face="bold", size=20))
-  g <- g + scale_y_continuous(limit = c(0, 60000),breaks = c(0,10000, 20000, 30000, 40000, 50000, 60000), labels = c(0,10,20,30,40,50,60))
-  g <- g + ggtitle(mydfnew.temp$tag_id)
+  g <- g + scale_y_continuous(limit = c(0, 30000),breaks = c(0,10000, 20000, 30000), labels = c(0,10,20,30))
+  g <- g + labs(title = mydfnew.temp$acoustic_tag_id, subtitle = mydfnew.temp$catch_year) 
   g <- g + ylab("Distance (km)")
   g <- g + xlab("Date")
+  g <- g + scale_x_datetime(date_breaks  ="1 month")
   g <- g + geom_hline(yintercept = mydfnew.temp$distance_to_source_m, colour = "gray", size = 0.5, linetype = "dashed")
-  g <- g + annotate("text",x = mydfnew.temp$arrival[1], y = mydfnew.temp$distance_to_source_m, label = mydfnew.temp$station_name, hjust=0, colour="red", size = 5)
+  g <- g + annotate("text",x = mydfnew.temp$arrival[1]- (240*60*60), y = mydfnew.temp$distance_to_source_m, label = mydfnew.temp$station_name, hjust=0, colour="red", size = 3)
   print(g)
 }
 
 dev.off()
+
 
 
 
