ef8429414d4019bd9556c43079a0517ffb3cc2c1
Pieterjan Verhelst <pieterjan.verhelst@inbo.be>
2022-04-13 21:46:04 +0200

    start implementing distance to source code

--- src/calculate_sourcedistance_function.R
+++ src/calculate_sourcedistance_function.R
@@ -4,15 +4,42 @@
 # hflavio@wlu.ca
 
 
+library(assertthat)
+library(dplyr)
 
-distanceSource <- function(movements, distance.method, dist.mat) {
+
+distanceSource <- function(movements,
+                           distance.method,
+                           dist.mat,
+                           upstream) {
   #appendTo("debug", "Running movementSpeeds.")
-  movements$distance_to_source_m[1] <- NA
+  
+  # get release station as a character (in case it would be a factor)
+  release_stat <- as.character(unique(movements$release_station))
+  assert_that(length(release_stat) == 1,
+              msg = "Only one release station per acoustic tag is allowed."
+  )
+  
+  # get upstream stations for the specific released fish
+  upstream_stations <- upstream %>%
+    filter(release_station == release_stat) %>%
+    pull(.data$upstream_stations)
+  
+  # initialize distance with sign
+  dist.mat_sign <- dist.mat
+  for (stat in upstream_stations) {
+    dist.mat_sign[stat, release_stat] <- dist.mat[stat, release_stat]*(-1)
+    dist.mat_sign[release_stat, stat] <- dist.mat[release_stat, stat]*(-1)
+  }
+  rm(dist.mat)
+  
+  # create column for collecting the distance to source
+  movements$distance_to_source_m <- NA
   if (nrow(movements) > 1) {
     capture <- lapply(2:nrow(movements), function(i) {
       if (movements$station_name[i] != movements$station_name[1] & all(!grep("^Unknown$", movements$station_name[(1):i]))) {
         if (distance.method == "last to first"){
-          dist <- dist.mat[movements$station_name[i], gsub(" ", ".", movements$station_name[1])]
+          dist <- dist.mat_sign[movements$station_name[i], movements$station_name[1]]
         }
         movements$distance_to_source_m[i] <<- round(dist, 6)
         rm(dist)
@@ -21,8 +48,10 @@ distanceSource <- function(movements, distance.method, dist.mat) {
       }
     })
   }
+  # remove release station column from output
+  movements$release_station <- NULL
+  
   return(movements)
 }
-
 
 
--- src/calculate_speed.R
+++ src/calculate_speed.R
@@ -25,12 +25,15 @@ distance_matrix <- read.csv("./data/external/distance_matrices/distancematrix_20
 # Calculate speed without taking into account different tag_id
 #speed <- movementSpeeds(residency, "last to first", distance_matrix)
 
-# Turn dataset into list per acoustic_tag_id
+# Turn dataset into list per tag_id
 residency_list <- split(residency , f = residency$acoustic_tag_id)
 #sapply(residency_list, function(x) max(x$detections))
 
-# Calculate speed per acoustic_tag_id
-speed_list <- lapply(residency_list, function(x) movementSpeeds(x, "last to first", distance_matrix))
+# Calculate speed per tag_id
+speed_list <- lapply(residency_list,
+                     function(x) {
+                       movementSpeeds(x, "last to first", distance_matrix)
+                     })
 #speed_list[[1]]
 
 # Turn lists back into dataframe
@@ -38,27 +41,65 @@ speed_list <- lapply(residency_list, function(x) movementSpeeds(x, "last to firs
 speed <- plyr::ldply (speed_list, data.frame)
 speed$.id <- NULL
 
-
 # Calculate total swim distance per acoustic_tag_id
 speed <- speed %>% 
   group_by(acoustic_tag_id) %>%
-  mutate(totaldistance_m=cumsum(coalesce(swimdistance_m, 0)) + swimdistance_m*0)
+  mutate(totaldistance_m=cumsum(coalesce(swimdistance_m, 0)))
 
 # Set row with release station at 0 total distance
 speed <- speed %>% 
-  #group_by(id) %>% 
-  mutate(totaldistance_m = ifelse(row_number() == 1 | all(is.na(totaldistance_m)), 0, totaldistance_m))
+  group_by(acoustic_tag_id) %>% 
+  mutate(totaldistance_m = ifelse(row_number() == 1 | all(is.na(totaldistance_m)), 0, totaldistance_m)) %>%
+  ungroup()
 
 # Replace the remaining NA values with previous non-NA value
 speed$totaldistance_m <- zoo::na.locf(speed$totaldistance_m)
 
-# Calculate the the station distance from a 'source' station
-speed <- distanceSource(speed, "last to first", distance_matrix)
+# Get upstream stations for each releasing station
+upstream_stations <- read_csv(here("data", "external", "station_order.csv"))
+upstream_stations_tidy <- upstream_stations %>%
+  separate_rows(.data$upstream_stations, sep = ", ")
+
+# check upstream stations 
+walk(unique(upstream_stations_tidy$upstream_stations), ~ 
+       assert_that(. %in% names(distance_matrix),
+                   msg = paste(., "is not present in the distance matrix."))
+)
+
+# Get release station for each fish
+release_stations <- 
+  speed %>%
+  distinct(acoustic_tag_id, station_name) %>%
+  mutate(is_a_releasing_station = str_detect(station_name, pattern = "rel")) %>%
+  filter(is_a_releasing_station == TRUE) %>%
+  select(-is_a_releasing_station) %>%
+  rename(release_station = station_name)
+
+# Check that all fishes have one and only one release_station
+nrow(release_stations) == length(unique(speed$acoustic_tag_id))
+
+# Add release station to speed
+speed <- speed %>%
+  left_join(release_stations, by = "acoustic_tag_id")
+
+# Calculate the distance to the station from a 'source' station
+nested_speed <- speed %>%
+  group_by(acoustic_tag_id) %>%
+  nest()
+
+speed <- nested_speed %>%
+  mutate(mov = map(data,
+                   distanceSource, 
+                   distance.method = "last to first",
+                   dist.mat = distance_matrix,
+                   upstream = upstream_stations_tidy)) %>%
+  select(acoustic_tag_id, mov) %>%
+  unnest(cols = c(mov))
 
 # Set 'NA' to '0'
 speed <- speed %>% 
   replace_na(list(distance_to_source_m = 0))
-                     
+
                      
 
 # Write csv
