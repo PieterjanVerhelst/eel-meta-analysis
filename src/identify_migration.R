7c0bba4cda5ada66309627072c166c4e401ac952
Damiano Oldoni <damiano.oldoni@inbo.be>
2023-05-10 21:53:17 +0200

    Add first draft for #17

---
+++ src/identify_migration.R
@@ -0,0 +1,88 @@
+#' Identify migration behavior
+#' 
+#' Damiano Oldoni (damiano.oldoni@inbo.be)
+#' Pieterjan Verhelst (pieterjan.verhelst@inbo.be)
+#' 
+#' R script for identifying eel migration behavior based on discussion in
+#' https://github.com/PieterjanVerhelst/eel-meta-analysis/issues/17
+#' 
+
+library(readr)      # to read csv files
+library(dplyr)      # to do data wrangling
+library(tidyr)      # to organize tabular data
+library(tidylog)    # to get useful messages about dplyr and tidyr operations
+library(purrr)      # to do functional programming
+library(assertthat) # to declare the conditions the code should satisfy
+library(lubridate)  # to work with datetime
+library(ggplot2)    # to create plots
+library(plotly)     # to make ggplot plots interactive
+
+
+# read input data
+eel_df <- read_csv("./data/interim/speed/speed_2013_albertkanaal.csv")
+eel_df <- eel_df %>%
+  rename(row_id = "...1")
+
+# define thresholds
+dist_for_speed <- 10000 # threshold in meter
+migration_speed_threshold <- 0.1 # speed threshold in m/s
+# t_threshold <- lubridate::dhours(x = 24) # 24 hours
+
+# customized `min()` function to avoid warning while calculating min() of empty
+# vectors
+custom_min <- function(x) {
+  if (length(x)>0) min(x) else NA
+}
+
+# define core function to find migration periods
+get_migrations <- function(df, 
+                           dist_threshold = dist_for_speed, 
+                           speed_threshold = migration_speed_threshold) {
+  df %>%
+    rowwise() %>%
+    mutate(first_dist_to_use = custom_min(
+      df$totaldistance_m[df$totaldistance_m >= dist_threshold])) %>%
+    mutate(row_first_dist_to_use = if_else(
+      !is.na(first_dist_to_use),
+      which(df$totaldistance_m == first_dist_to_use)[1],
+      NA)) %>%
+    ungroup() %>%
+    mutate(time_first_dist_to_use = if_else(!is.na(row_first_dist_to_use),
+                                        df$arrival[row_first_dist_to_use],
+                                        NA)) %>%
+    mutate(delta_totdist = first_dist_to_use - totaldistance_m) %>%
+    mutate(delta_t = as.numeric(as.duration(time_first_dist_to_use - departure))) %>%
+    mutate(migration_speed = (delta_totdist / delta_t)) %>%
+    mutate(downstream_migration = migration_speed >= speed_threshold)
+}
+
+# calculate dist + dist_for_speed
+eel_df <- eel_df %>%
+  mutate(dist_threshold = totaldistance_m + dist_for_speed)
+
+# Apply get_migrations to each eel
+eel_df <- eel_df %>%
+  group_by(acoustic_tag_id) %>%
+  nest() %>%
+  mutate(migration_infos = map(data, function(x) {
+    get_migrations(x, 
+                   dist_threshold = dist_for_speed, 
+                   migration_speed_threshold)
+    }), .keep = "none") %>%
+  unnest(migration_infos)
+
+View(eel_df)
+
+# select one eel
+acoustic_tag_id_example <- "A69-1601-26515"
+eel_example <- eel_df %>%
+  filter(acoustic_tag_id == acoustic_tag_id_example)
+# plot
+plot_example <- ggplot(eel_example, aes(x = arrival, y = totaldistance_m)) +
+  geom_point(aes(color = downstream_migration)) +
+  geom_line() + 
+  scale_y_reverse()
+plot_example
+
+# interactive plot version
+ggplotly(plot_example)
