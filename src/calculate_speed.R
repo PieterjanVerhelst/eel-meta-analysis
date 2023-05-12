0b15ce6f06ac6eff0c311f213ac9989539b96f1b
Pieterjan Verhelst <pieterjan.verhelst@inbo.be>
2023-05-12 15:36:55 +0200

    source clean residency data
    move cleaning code to separate script

--- src/calculate_speed.R
+++ src/calculate_speed.R
@@ -23,35 +23,8 @@ residency <- read_csv(
 residency$...1 <- NULL
 residency$acoustic_tag_id <- factor(residency$acoustic_tag_id)
 
-# CLEAN DATA ####
-# Remove false detections 
-# ESGL
-# False detection in project ESGL after 2016-02-15 and the detection of eel A69-1601-38319 at station A on 2016-01-02 06:59:09
-residency <- residency[!(residency$animal_project_code == "ESGL" & residency$arrival >= '2016-02-15 00:00:00'),]
-residency <- residency[!(residency$animal_project_code == "ESGL" & residency$acoustic_tag_id == "A69-1601-38319" &
-                        residency$arrival >= '2016-01-02 06:59:09' &
-                          residency$arrival <= '2016-01-02 07:59:09'),]
-
-# 2011_warnow
-# False detections of station W3 in 2011 warnow project: detections at W3 same day as release, while W3 is ca. 25 km downstream of release
-# see github issue https://github.com/PieterjanVerhelst/eel-meta-analysis/issues/20
-residency$date <- as.Date(residency$arrival)
-residency <- residency %>%
-  group_by(acoustic_tag_id) %>%
-  mutate(start_date = dplyr::first(date)) %>%
-  filter((station_name != "W3") | (station_name == "W3" & date != start_date)) %>%
-  filter((station_name != "W4") | (station_name == "W4" & date != start_date))
-
-residency$date <- NULL
-residency$start_date <- NULL
-
-# Remove wrong release location in 2013_albertkanaal
-# No need to adjust exact release location, since eels were released next to receiver, leading to detection right after release
-# --> exact release positions was HH5 instead of rel_albertkanaal2
-residency <- subset(residency, station_name != "rel_albertkanaal2")
-#################
-
-
+# Clean residency data
+source("./src/clean_residency_data.R")
 
 # Load distance matrix
 # Make sure the first column is not containing the station names
