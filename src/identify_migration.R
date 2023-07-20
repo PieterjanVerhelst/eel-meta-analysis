879c71dfabe8ad86d67f9507f95376ffa5136fe6
Pieterjan Verhelst <pieterjan.verhelst@inbo.be>
2023-07-20 10:15:27 +0200

    calculate migration nedap meuse

--- src/identify_migration.R
+++ src/identify_migration.R
@@ -18,12 +18,13 @@ library(plotly)     # to make ggplot plots interactive
 source("src/identify_migration_functions.R")
 
 # read input data
-animal_project_id <- "2012_leopoldkanaal"
+animal_project_id <- "noordzeekanaal"
 eel_df <- read_csv(
   sprintf("./data/interim/speed/speed_%s.csv", animal_project_id)
 )
 eel_df <- eel_df %>%
   rename(row_id = "...1")
+
 
 # define thresholds
 dist_for_speed <- 4000 # threshold in meter
@@ -44,7 +45,7 @@ eel_df <- eel_df %>%
 
 
 # select one eel
-acoustic_tag_id_example <- "A69-1601-52665"
+acoustic_tag_id_example <- "A69-1601-18213"
 eel_example <- eel_df %>%
   filter(acoustic_tag_id == acoustic_tag_id_example)
 # plot
