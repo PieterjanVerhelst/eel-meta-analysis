1dbbd70e0d8329c005b3f33cf976a6fca28c88cb
Damiano Oldoni <damiano.oldoni@inbo.be>
2023-10-11 17:04:37 +0200

    Fix #45

--- src/identify_migration.R
+++ src/identify_migration.R
@@ -18,7 +18,7 @@ library(plotly)     # to make ggplot plots interactive
 source("src/identify_migration_functions.R")
 
 # read input data
-animal_project_id <- "2015_phd_verhelst_eel"
+animal_project_id <- "semp"
 eel_df <- read_csv(
   sprintf("./data/interim/speed/speed_%s.csv", animal_project_id)
 )
@@ -29,6 +29,7 @@ eel_df <- eel_df %>%
 # define thresholds
 dist_for_speed <- 4000 # threshold in meter
 migration_speed_threshold <- 0.01 # speed threshold in m/s
+stationary_range <- 150 #threshold in meter defining what we consider as "stationary"
 
 # Apply get_migrations to each eel
 eel_df <- eel_df %>%
@@ -37,7 +38,8 @@ eel_df <- eel_df %>%
   mutate(migration_infos = map(data, function(x) {
     get_migrations(x, 
                    dist_threshold = dist_for_speed,
-                   speed_threshold = migration_speed_threshold)
+                   speed_threshold = migration_speed_threshold,
+                   smooth_threshold = stationary_range)
   }), .keep = "none") %>%
   unnest(migration_infos)
 
