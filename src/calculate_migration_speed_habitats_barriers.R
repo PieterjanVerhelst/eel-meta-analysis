8f212fcc0484aa48e035bd718e5343e2be4018d2
Pieterjan Verhelst <pieterjan.verhelst@inbo.be>
2023-12-13 17:27:37 +0100

    change animal project code names into waterbody names

--- src/calculate_migration_speed_habitats_barriers.R
+++ src/calculate_migration_speed_habitats_barriers.R
@@ -30,27 +30,52 @@ migration_speed$time_days <- migration_speed$time / (60*60*24)
 summary(migration_speed$time_days)
 boxplot(migration_speed$speed_ms, ylab = "Migration speed (m/s)")
 
+
+# Rename animal_project_code to river or estuary names
+migration_speed$animal_project_code <- recode_factor(migration_speed$animal_project_code, 
+                                                     'mondego' = "Mondego",
+                                                     'esgl' = "Grand Lieu Lake",
+                                                     '2011_loire' = "Loire",
+                                                     '2014_frome' = "Frome",
+                                                     '2012_leopoldkanaal' = "Leopold Canal",
+                                                     '2015_phd_verhelst_eel' = "Scheldt",
+                                                     'dak_markiezaatsmeer' = "Markiezaatsmeer",
+                                                     '2019_grotenete' = "Grote Nete",
+                                                     '2013_albertkanaal' = "Albert Canal",
+                                                     'nedap_meuse' = "Meuse",
+                                                     '2013_stour' = "Stour",
+                                                     'noordzeekanaal' = "Noordzeekanaal",
+                                                     '2014_nene' = "Nene",
+                                                     'dak_superpolder' = "Suderpolder",
+                                                     '2004_gudena' = "Gudena",
+                                                     '2011_warnow' = "Warnow",
+                                                     'semp' = "Nemunas",
+                                                     'emmn' = "Alta")
+
+
+
+
 # Create boxplot with speeds according to habitat type per project in geographical order from west to east 
 migration_speed$animal_project_code <- factor(migration_speed$animal_project_code, 
-                                              levels = c("mondego",
-                                                         "esgl",
-                                                         "2011_loire",
+                                              levels = c("Mondego",
+                                                         "Grand Lieu Lake",
+                                                         "Loire",
                                                          #"2017_fremur",
-                                                         "2014_frome",
-                                                         "2012_leopoldkanaal",
-                                                         "2015_phd_verhelst_eel",
-                                                         "dak_markiezaatsmeer",
-                                                         "2019_grotenete",
-                                                         "2013_albertkanaal",
-                                                         "nedap_meuse",
-                                                         "2013_stour",
-                                                         "noordzeekanaal",
-                                                         "2014_nene",
-                                                         "dak_superpolder",
-                                                         "2004_gudena",
-                                                         "2011_warnow",
-                                                         "semp",
-                                                         "emmn"))
+                                                         "Frome",
+                                                         "Leopold Canal",
+                                                         "Scheldt",
+                                                         "Markiezaatsmeer",
+                                                         "Grote Nete",
+                                                         "Albert Canal",
+                                                         "Meuse",
+                                                         "Stour",
+                                                         "Noordzeekanaal",
+                                                         "Nene",
+                                                         "Suderpolder",
+                                                         "Gudena",
+                                                         "Warnow",
+                                                         "Nemunas",
+                                                         "Alta"))
 
 aggregate(migration_speed$speed_ms, list(migration_speed$habitat_type3), mean)
 aggregate(migration_speed$speed_ms, list(migration_speed$animal_project_code, migration_speed$habitat_type3), mean)
@@ -96,4 +121,154 @@ migration_speed_plot <- ggplot(migration_speed, aes(x=animal_project_code, y=spe
     axis.text.y = element_text(size = 22, colour = "black"),
     axis.title.y = element_text(size = 22))
 migration_speed_plot
+
+
+# 2. Analyse migration speed in relation to migration barriers ####
+# Merge migration barrier qualification to the dataset
+migrationbarriers <- read_csv("./data/external/migrationbarriers.csv")
+migrationbarriers <- migrationbarriers %>%
+  mutate_at(c('animal_project_code', 'barrier_impact', 'weir', 'sluice_gate', 'shipping_lock', 'hydropower', 'pump', 'barrier_type'), as.factor) %>%
+  select(-'barrier_type')
+migration_speed <- left_join(migration_speed, migrationbarriers, by = "animal_project_code")
+
+# Part of semp-eels had free migration route and another part went through hydropower
+semp_data <- read.csv('./data/interim/migration/migration_semp.csv')
+eel_semp_free <- semp_data %>%
+  mutate_at(c('acoustic_tag_id', 'animal_project_code', 'station_name'), as.factor) %>%
+  filter(station_name == "rel_semp1" |
+           station_name == "rel_semp2"|
+           station_name == "rel_semp3"|
+           station_name == "rel_semp5"|
+           station_name == "rel_semp6") %>%
+  select(animal_project_code, acoustic_tag_id, station_name)
+eel_semp_free <- eel_semp_free$acoustic_tag_id
+
+# Classify barrier type
+migration_speed$barrier_type <- NA
+migration_speed <- migration_speed %>%
+  mutate(barrier_type = case_when(animal_project_code == "semp" & acoustic_tag_id %in% eel_semp_free ~ "none",
+                                  animal_project_code == "semp" ~ "hydropower",
+                                  animal_project_code == "mondego" ~ "weir",
+                                  animal_project_code == "esgl" ~ "weir",
+                                  animal_project_code == "2011_loire" ~ "none",
+                                  animal_project_code == "2014_frome" ~ "weir",
+                                  animal_project_code == "2012_leopoldkanaal" ~ "pump",
+                                  animal_project_code == "2015_phd_verhelst_eel" ~ "none",
+                                  animal_project_code == "dak_markiezaatsmeer" ~ "weir",
+                                  animal_project_code == "2019_grotenete" ~ "none",
+                                  animal_project_code == "2013_albertkanaal" ~ "shipping_lock",
+                                  animal_project_code == "nedap_meuse" ~ "hydropower",
+                                  animal_project_code == "2013_stour" ~ "weir",
+                                  animal_project_code == "noordzeekanaal" ~ "pump",
+                                  animal_project_code == "2014_nene" ~ "weir",
+                                  animal_project_code == "dak_superpolder" ~ "shipping_lock",
+                                  animal_project_code == "2004_gudena" ~ "none",
+                                  animal_project_code == "2011_warnow" ~ "weir",
+                                  animal_project_code == "emmn" ~ "none"))
+
+aggregate(migration_speed$speed_ms, list(migration_speed$barrier_impact), mean)
+aggregate(migration_speed$speed_ms, list(migration_speed$barrier_type), mean)
+
+
+# Impact of migration barriers is only relevant in non-tidal freshwater systems
+migration_speed_freshwater <- filter(migration_speed, habitat_type3 == "freshwater")
+  
+  
+#  Plot speed in relation to those barrier qualification
+migration_speed_plot <- ggplot(migration_speed_freshwater, aes(x=barrier_impact, y=speed_ms)) + 
+  geom_boxplot() +
+  ylab("Migration speed (m/s)") + 
+  xlab("Qualitative migration barrier impact") +
+  stat_summary(fun = "mean", geom = "point", #shape = 8,
+               size = 4, color = "blue") +
+  theme( 
+    panel.grid.major = element_blank(), 
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(), 
+    axis.line = element_line(colour = "black"),
+    axis.text.x = element_text(size = 16, colour = "black", angle=360),
+    axis.title.x = element_text(size = 22),
+    axis.text.y = element_text(size = 22, colour = "black"),
+    axis.title.y = element_text(size = 22))
+migration_speed_plot
+
+# Colour boxplots in relation to barrier impact
+migration_speed_plot <- ggplot(migration_speed_freshwater, aes(x=animal_project_code, y=speed_ms, fill = barrier_impact)) + 
+  geom_boxplot() +
+  scale_fill_brewer(palette="Dark2") +
+  ylab("Migration speed (m/s)") + 
+  xlab("Animal project code") +
+  stat_summary(fun = "mean", geom = "point", #shape = 8,
+               size = 4, color = "blue", show.legend = FALSE) +
+  theme( 
+    panel.grid.major = element_blank(), 
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(), 
+    axis.line = element_line(colour = "black"),
+    axis.text.x = element_text(size = 16, colour = "black", angle=90),
+    axis.title.x = element_text(size = 22),
+    axis.text.y = element_text(size = 22, colour = "black"),
+    axis.title.y = element_text(size = 22))
+migration_speed_plot
+
+# Colour boxplots in relation to barrier type
+migration_speed_plot <- ggplot(migration_speed_freshwater, aes(x=animal_project_code, y=speed_ms, fill = barrier_type)) + 
+  geom_boxplot() +
+  scale_fill_brewer(palette="Dark2") +
+  ylab("Migration speed (m/s)") + 
+  xlab("Animal project code") +
+  stat_summary(fun = "mean", geom = "point", #shape = 8,
+               size = 4, color = "blue", show.legend = FALSE) +
+  theme( 
+    panel.grid.major = element_blank(), 
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(), 
+    axis.line = element_line(colour = "black"),
+    axis.text.x = element_text(size = 16, colour = "black", angle=90),
+    axis.title.x = element_text(size = 22),
+    axis.text.y = element_text(size = 22, colour = "black"),
+    axis.title.y = element_text(size = 22))
+migration_speed_plot
+
+
+# 3. Analyse migration speed in non-tidal, freshwater area in relation to barrier type and geographical location ####
+# Join geographical location (i.e. latitude) to dataset
+lat <- read_csv("./data/external/project_geographical_location.csv")
+lat$animal_project_code <- factor(lat$animal_project_code)
+
+migration_speed_freshwater <- left_join(migration_speed_freshwater, lat, by = "animal_project_code")
+plot(migration_speed_freshwater$speed_ms ~ migration_speed_freshwater$latitude)
+
+
+
+
+
+
+
+# 4. Analyse migration speed in tidal area in relation to geographical location ####
+migration_speed_tidal <- filter(migration_speed, habitat_type3 == "tidal")
+lat <- read_csv("./data/external/project_geographical_location.csv")
+lat$animal_project_code <- factor(lat$animal_project_code)
+
+migration_speed_tidal <- left_join(migration_speed_tidal, lat, by = "animal_project_code")
+plot(migration_speed_tidal$speed_ms ~ migration_speed_tidal$latitude)
+
+migration_speed_tidal_plot <- ggplot(migration_speed_tidal, aes(x=animal_project_code, y=speed_ms)) + 
+  geom_boxplot() +
+  #scale_fill_brewer(palette="Dark2") +
+  ylab("Migration speed (m/s)") + 
+  xlab("Animal project code") +
+  stat_summary(fun = "mean", geom = "point", #shape = 8,
+               size = 4, color = "blue", show.legend = FALSE) +
+  theme( 
+    panel.grid.major = element_blank(), 
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(), 
+    axis.line = element_line(colour = "black"),
+    axis.text.x = element_text(size = 16, colour = "black", angle=90),
+    axis.title.x = element_text(size = 22),
+    axis.text.y = element_text(size = 22, colour = "black"),
+    axis.title.y = element_text(size = 22))
+migration_speed_tidal_plot
+
 
