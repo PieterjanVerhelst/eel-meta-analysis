b2bd221d0a5f0938a9cf4f0e0c8365e15a925ebf
Pieterjan Verhelst <pieterjan.verhelst@inbo.be>
2023-12-20 13:48:08 +0100

    analyse geographical location effect on migration period

--- README.md
+++ README.md
@@ -78,13 +78,13 @@ For this meta-analysis, data from 20 projects/locations and 9 countries have bee
 	+ 11a. `identify_migration_functions.R`: functions to identify migration.
 12. `create_distance_plot.R`: Create plots with travelled distance per eel and store as .pdf
 13. `process_migration_data.R`: Process migration data by combining the datasets of the different studies and calculate the number of migratory eels per study
-14. `calculate_migration_speed_overall.R`: Calculate the migration speed as the speed between the first and last detection and relate it to geographical position, habitat, size and sex
-15. `calculate_migration_speed_habitats.R`: Calculate the migration speed according to habitat type and analyse difference between tidal vs non-tidal habitats
-16. `analyse_migration_speed_tidal.R`: Analyse the migration speed in **tidal areas** according to size, sex and geographical location
-17. `analyse_migration_speed_nontidal.R`: Analyse the migration speed in **non-tidal areas** according to size, sex, geographical location, migration barrier number and type
+14. `analyse_migration_period.R`: Analyse the moment (day of year) when eels started to migrate in relation to size and geographical location
+15. `calculate_migration_speed_overall.R`: Calculate the migration speed as the speed between the first and last detection and relate it to geographical position, habitat, size and sex
+16. `calculate_migration_speed_habitats.R`: Calculate the migration speed according to habitat type and analyse difference between tidal vs non-tidal habitats
+17. `analyse_migration_speed_tidal.R`: Analyse the migration speed in **tidal areas** according to size, sex and geographical location
+18. `analyse_migration_speed_nontidal.R`: Analyse the migration speed in **non-tidal areas** according to size, sex, geographical location, migration barrier number and type
 
 
-xx.  `analyse_migration_period.R`: Analyse the moment (day of year) when eels started to migrate in relation to size and geographical location
 
 ### Figures
 
--- src/analyse_migration_period.R
+++ src/analyse_migration_period.R
@@ -80,20 +80,45 @@ period$daynumber <- factor(period$daynumber)
 
 # Calculate summary
 period_summary <- period %>%
-  #group_by(animal_project_code, daynumber) %>%
-  group_by(daynumber) %>%
+  group_by(animal_project_code, daynumber) %>%
+  #group_by(daynumber) %>%
    count()
 
 # Create dataset for barplot
-plot_data <- data.frame (daynumber  = 1:366)
+plot_data <- data.frame (daynumber  = 1:365)
 plot_data$daynumber <- factor(plot_data$daynumber)
 plot_data <- left_join(plot_data, period_summary, by = "daynumber")
 plot_data <- replace(plot_data, is.na(plot_data), 0)  # Replace NAs with 0s
+plot_data$daynumber <- as.numeric(plot_data$daynumber)
 
+plot_data_project <- filter(plot_data, animal_project_code == "Meuse")
 
 # Create barplot with number of eels per day number
-ggplot(period_summary, aes(x=daynumber, y=n)) + 
-  geom_bar(stat="identity") +
+waterbodies <- c("Mondego", "Grand Lieu Lake", "Loire", "Frome", "Leopold Canal", "Scheldt", "Markiezaatsmeer", "Grote Nete", "Albert Canal", "Meuse", "Stour", "Noordzeekanaal", "Nene", "Suderpolder", "Gudena", "Warnow", "Nemunas", "Alta") # This removes 'NA' from legend
+
+ggplot(plot_data, aes(x=daynumber, y=n, fill = animal_project_code)) + 
+  geom_bar(stat="identity", width = 1) +
+  #scale_fill_brewer(palette="Dark2") +
+  scale_fill_manual(values=c("#9933FF", 
+                             "#33FFFF", 
+                             "darkred", 
+                             "darkblue",
+                             "darkgreen",
+                             "orange",
+                             "yellow",
+                             "#70ef2d",
+                             "#652A0E",
+                             "grey57",
+                             "pink",
+                             "deeppink",
+                             "#276DC2",
+                             "red",
+                             "black",
+                             "honeydew3",
+                             "magenta",
+                             "lightcyan"
+                             ),
+                    limits = waterbodies) +
   ylab("Number of eels") + 
   xlab("Day of the year") +
   theme( 
@@ -104,10 +129,59 @@ ggplot(period_summary, aes(x=daynumber, y=n)) +
     axis.text.x = element_text(size = 16, colour = "black", angle=360),
     axis.title.x = element_text(size = 22),
     axis.text.y = element_text(size = 22, colour = "black"),
-    axis.title.y = element_text(size = 22))
+    axis.title.y = element_text(size = 22)) +
+  scale_x_continuous(breaks = seq(0, 365, by = 30))
 
 
 
+# 3. Size analysis  ####
+
+# Plot
+period$daynumber <- as.numeric(period$daynumber)
+
+ggplot(period, aes(x= daynumber, y=length1)) + 
+  geom_point() +
+  facet_wrap(~animal_project_code, scales = "free") +
+  ylab("Total length (mm)") + 
+  xlab("Day of the year") +
+  #stat_summary(fun = "mean", geom = "point", #shape = 8,
+  #             size = 4, color = "blue") +
+  theme( 
+    panel.grid.major = element_blank(), 
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(), 
+    axis.line = element_line(colour = "black"),
+    axis.text.x = element_text(size = 12, colour = "black", angle=360),
+    axis.title.x = element_text(size = 12),
+    axis.text.y = element_text(size = 12, colour = "black"),
+    axis.title.y = element_text(size = 12)) +
+  scale_x_continuous(breaks = seq(0, 365, by = 30)) +
+  geom_smooth(method='lm')
+
+
+
+
+# 4. Geographical location analysis ####
+
+# Plot
+period$daynumber <- as.numeric(period$daynumber)
+
+ggplot(period, aes(x= release_latitude , y=daynumber)) + 
+  geom_point() +
+  ylab("Day of the year") + 
+  xlab("Release latitude") +
+  #stat_summary(fun = "mean", geom = "point", #shape = 8,
+  #             size = 4, color = "blue") +
+  theme( 
+    panel.grid.major = element_blank(), 
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(), 
+    axis.line = element_line(colour = "black"),
+    axis.text.x = element_text(size = 12, colour = "black", angle=360),
+    axis.title.x = element_text(size = 12),
+    axis.text.y = element_text(size = 12, colour = "black"),
+    axis.title.y = element_text(size = 12)) +
+  geom_smooth(method='lm')
 
 
 
